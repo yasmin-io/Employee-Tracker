b365870b5700b7a67b65bfcb2959b31c030471da
Yasmin <yasmin.bunnie@gmail.com>
2022-03-18 01:17:05 +0000

    created seed.sql info for roles and employee

--- db/seed.sql
+++ db/seed.sql
@@ -1,11 +1,27 @@
 use employees;
 
 INSERT INTO departments (name)
-VALUES
-    ('Finance'),
-    ('Sales'), 
-    ('Legal'),
-    ('Service'),
-    ('Engineering');
+VALUES ('Finance'),
+       ('Sales'), 
+       ('Legal'),
+       ('Service'),
+       ('Engineering');
 
+
+
+
+INSERT INTO roles(title, salary)
+VALUES ("Sales Lead", "100000"),
+       ("Salesperson", "80000"),
+       ("Lead Engineer", "150000"),
+       ("Software Engineer", "120000"),
+       ("Account Manager", "160000"),
+       ("Accountant", "125000"),
+       ("Legal Team Lead", "250000"),
+       ("Lawyer", "190000");
+
+
+INSERT INTO employee(first_name, last_name)
+VALUES ("Yasmin", "Ali"),
+       ("Josh", "Rose"),
     
