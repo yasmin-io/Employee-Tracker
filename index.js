b136af5e335d1bbb954e8b867b3724000ddb3972
Yasmin <yasmin.bunnie@gmail.com>
2022-03-24 02:24:46 +0000

    created a logo when application is run

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 // Requiring Inquirer to prompt the user with the questions we want to ask.
-const { type } = require("express/lib/response");
+const express = require("express");
 const inquirer = require("inquirer");
+// This is what we require to create our logos that display when you first run the command line application
+const logo = require("asciiart-logo");
+
+function init() {
+  // logo is a function provided by asciiart-logo and you can provide an object array of text title, text description and styling.
+  console.log(logo({ name: "Manager Page", font: "Speed" }).render());
+}
+
 // ..
 const dbStore = require("./db");
+// const { init } = require("express/lib/application");
+// const { type } = require("express/lib/response");
+
 // Console.table allows us to view tabular data as tables in the terminal.
 // Similar to how console.log will display strings or javascript objects in the console.
 require("console.table");
@@ -26,25 +37,11 @@ const mainOptions = () => {
           "Update Employee Role",
         ],
       },
-      {
-        type: "list",
-        name: "otheroptions",
-        message: "What would you like to do?",
-        choices: [
-          "View All Departments",
-          "View All Roles",
-          "View All Employees",
-          "Add Department",
-          "Add Role",
-          "Add Employee",
-          "Update Employee Role",
-        ],
-      },
     ])
     .then((userChoice) => {
       // If the user's Choice matches the if statement, then execute the block of code attatched.
       if (userChoice.options === "View All Departments") {
-        console.log(userChoice);
+        // console.log(userChoice);
         console.log("You picked option number 1!");
         viewDepartments();
       }
@@ -63,4 +60,6 @@ function viewDepartments() {
 
 //Create all options here
 
+// Run these functions when the user types ' node index.js'
+init();
 mainOptions();
